66843fa2ba6894bb64f28d631d76ffc62bae4c8c
Olga Kravchenko <olga19972010@yandex.by>
2020-12-06 18:43:34 +0300

    9 Add JS in modals windows

---
+++ js/modal.js
@@ -0,0 +1,57 @@
+const buttonWrite = document.querySelector('.button--write-us');
+const writeUsSection = document.querySelector('.section-wright-us');
+const writeUsClose = document.querySelector('.button--wright-us-close');
+const formWriteUs = document.querySelector('.form-write-us');
+const inputName = document.querySelector('.input-name');
+const inputEmail  = document.querySelector('.input-email');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+buttonWrite.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  writeUsSection.classList.add('section-wright-us--show');
+
+  if (storage) {
+    inputName.value = storage;
+    inputEmail.focus();
+  } else {
+    inputName.focus();
+  }
+});
+
+writeUsClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  writeUsSection.classList.remove('section-wright-us--show');
+  writeUsSection.classList.remove('section-wright-us--error');
+});
+
+formWriteUs.addEventListener(`submit`, function (evt) {
+  console.log(`Попытка`)
+  if (!(inputName.value && inputEmail.value)) {
+    evt.preventDefault();
+    writeUsSection.classList.remove('section-wright-us--error');
+    writeUsSection.offsetWidth = writeUsSection.offsetWidth;
+    writeUsSection.classList.add('section-wright-us--error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem(`name`, inputName.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (writeUsSection.classList.contains(`form-section--show`)) {
+      evt.preventDefault();
+      writeUsSection.classList.remove(`form-section--show`);
+      writeUsSection.classList.remove("form-section--error");
+    }
+  }
+});
